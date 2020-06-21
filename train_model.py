4f9d134e62992db400af276fdfb5ab65204fa32c
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2020-06-21 23:55:52 +0600

    commit-1.0

---
+++ train_model.py
@@ -0,0 +1,56 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten,LSTM
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+# more info on callbakcs: https://keras.io/callbacks/ model saver is cool too.
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+import  tensorflow as tf
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+dense_layer = '0'
+layer_size = '64'
+conv_layer = '3'
+
+
+
+
+NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+print(NAME)
+
+model = Sequential()
+
+# convolutional layer
+model.add(Conv2D(50, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu', input_shape=(28, 28, 1)))
+
+# convolutional layer
+model.add(Conv2D(75, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(125, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+# flatten output of conv
+model.add(Flatten())
+
+# hidden layer
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(250, activation='relu'))
+model.add(Dropout(0.3))
+# output layer
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+tensorboard = TensorBoard(log_dir="logs\\{}".format(NAME))
+
+model.fit(X, y, batch_size=32, epochs=10, callbacks=[tensorboard])
+
+model.save('64x3-CNN.model')
